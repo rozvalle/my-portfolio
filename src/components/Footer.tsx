89307080b380f913bf90a467ad9e42ab6148f5bd
Rozvalle <elmovaleros11@gmail.com>
2025-12-14 14:14:51 +0800

    feat: remove unused components and update routing with animated transitions

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-gray-900 text-white text-center p-6">
-            <p>© 2025 Francis Elmo V. All rights reserved.</p>
-        </footer>
-    );
-}
